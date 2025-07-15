1e1061bc63e33dfeee993f4343894253b5b309e3
Seyman08 <oluwaseyioyekale8@gmail.com>
2025-07-15 12:43:57 -0700

    comment out resend.ts and also in route.ts

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,18 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
-import { resend } from "@/lib/resend";
+// import { resend } from "@/lib/resend";
 
 export async function GET(req: NextRequest) {
   try {
-    const { data, error } = await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to: "sanmi.hq@gmail.com",
-      subject: "Hello world",
-      html: "<h1>Hello world from sender</h1>",
-    });
+    // const { data, error } = await resend.emails.send({
+    //   from: "onboarding@resend.dev",
+    //   to: "sanmi.hq@gmail.com",
+    //   subject: "Hello world",
+    //   html: "<h1>Hello world from sender</h1>",
+    // });
 
-    if (error) {
-      return NextResponse.json({ error }, { status: 500 });
-    }
+    // if (error) {
+    //   return NextResponse.json({ error }, { status: 500 });
+    // }
 
     return NextResponse.json(data);
   } catch (error) {
